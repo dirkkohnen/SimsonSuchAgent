df3f4a3bc1f165b9d2fc8cfadee67f763d33ec9c
dirkkohnen <dkohnen@me.com>
2016-03-01 09:51:54 +0100

    Erster Commit

---
+++ SimsonSuchAgent/src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author dirk.kohnen
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Automatisch generierter Methodenstub
+		System.out.println("Hallo Welt");
+
+	}
+
+}
