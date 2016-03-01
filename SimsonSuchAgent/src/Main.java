e0763ef53bde2a9fb9b8aea070a49a5357e80efa
dirkkohnen <dkohnen@me.com>
2016-03-01 09:54:14 +0100

    Zweiter Commit

--- SimsonSuchAgent/src/Main.java
+++ SimsonSuchAgent/src/Main.java
@@ -14,6 +14,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Automatisch generierter Methodenstub
 		System.out.println("Hallo Welt");
+		System.out.println("Hallo Du");
 
 	}
 
