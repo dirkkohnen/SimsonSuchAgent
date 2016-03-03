b606436b6a37ad7f36e9781a712cdc5038a375cf
dirkkohnen <dkohnen@me.com>
2016-03-03 08:41:20 +0100

    -useragnet & Timeout eingebaut -Link AKF geändert

--- SimsonSuchAgent/src/de/kohnen/sontje/Main.java
+++ SimsonSuchAgent/src/de/kohnen/sontje/Main.java
@@ -17,7 +17,7 @@ import org.jsoup.select.Elements;
  */
 public class Main {
 
-	private static String shopPath = "http://akf-shop.shopgate.com/search?s=&sort=relevance_desc&page=";
+	private static String shopPath = "http://akf-shop.shopgate.com/category_show_all_items/353631?cat_id=5342586&list_category=Simson+Ersatzteile&sort=relevance_desc&page=";
 	private static int DEBUG = 1; //Bestimmt die debuglevel; 0=Fehler; 1=Einfache Statusmeldung; 2=Detailierte Statusmeldung; 
 	private Elements elements;
 	private int nArtikel = 0;
@@ -40,13 +40,15 @@ public class Main {
 		Main.debug(1, "Starte Methode 'getArtikel()'");
 		try {
 			for (int i = 1; i <= 708 ; i++ ){
-				Document doc = Jsoup.connect(shopPath+i).get();
+				Document doc = Jsoup.connect(shopPath+i).userAgent("Mozilla").timeout(10000).get();
 				this.elements = doc.getElementsByClass("product-title");
 				for (Element el : this.elements) {
 					nArtikel ++;
 					//System.out.println(el.text());
 				}
 				nPages++;
+				Main.debug(1, "auslesen seite " + nPages + " abgeschlossen");
+			
 			}
 			Main.debug(1, nPages + " Seiten ausgelesen");
 			Main.debug(1, nArtikel + " Artikel erkannt");
