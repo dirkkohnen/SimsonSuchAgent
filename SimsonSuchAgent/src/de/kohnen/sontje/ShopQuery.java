672e04c944fa5aec46667536822b098c7ec51ae5
dirkkohnen <dkohnen@me.com>
2016-03-07 17:12:44 +0100

    - Zugriff von aussen auf Datenbank - Insertbefehl eingefügt
    - ToDo: Fehler bei Insertbefehl beheben

--- SimsonSuchAgent/src/de/kohnen/sontje/ShopQuery.java
+++ SimsonSuchAgent/src/de/kohnen/sontje/ShopQuery.java
@@ -5,41 +5,54 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ShopQuery {
+		private Connection con = null;
+		private Statement st = null;
+		private int rs = 0;
 
+        private String url = "jdbc:mysql://kohnen.selfip.com/sontje_dev";
+        private String user = "root";
+        private String password = "i4HgAZ3rbT7n8ir7fGkXvwEVZ";
+        //INSERT INTO `sontje_dev`.`QueryLog` (`ID`, `shopID`, `start`, `ende`, `nSeiten`, `nArtikel`, `status`, `meldung`) VALUES (NULL, '%d', %s, '%s', '%d', '%d', '%s', '%s');";
+        private String queryString = "";
+        
+        private int shopID = 1;
+        private Date start = new Date();
+        private Date ende = null;
+        private int nSeiten = 0;
+        private int nArtikel = 0;
+        private String status = "abgeschlossen";
+        private String meldung = "Erster Test mit der KLasse ShopQuery";
+        
 	/**
 	 * Constructor
 	 */
 	public ShopQuery(){
-        Connection con = null;
-        Statement st = null;
-        ResultSet rs = null;
-
-        String url = "jdbc:mysql://192.168.151.21:3306/sontje_dev";
-        String user = "root";
-        String password = "i4HgAZ3rbT7n8ir7fGkXvwEVZ";
 
         try {
             con = DriverManager.getConnection(url, user, password);
             st = con.createStatement();
-            rs = st.executeQuery("SELECT VERSION()");
+            ende = new Date();
+            queryString = String.format("INSERT INTO `sontje_dev`.`QueryLog` (`ID`, `shopID`, `start`, `ende`, `nSeiten`, `nArtikel`, `status`, `meldung`) VALUES (NULL, '" + shopID + "', '" + start.getTime() + "', '" + ende.getTime() + "', '" + nSeiten + "', '" + nArtikel + "', '" + status + "', '" + meldung + "');");
+            rs = st.executeUpdate(queryString);
 
-            if (rs.next()) {
+ /*           if (rs.next()) {
                 System.out.println(rs.getString(1));
             }
-
+*/
         } catch (SQLException ex) {
             Logger lgr = Logger.getLogger(ShopQuery.class.getName());
             lgr.log(Level.SEVERE, ex.getMessage(), ex);
 
         } finally {
             try {
-                if (rs != null) {
+/*                if (rs != null) {
                     rs.close();
-                }
+                }*/
                 if (st != null) {
                     st.close();
                 }
